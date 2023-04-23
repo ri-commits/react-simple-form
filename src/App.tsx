4403f5e82615e24183e76e900d6dd03849446f36
ri-commits <kartashourychard@gmail.com>
2023-04-23 16:13:37 +0200

    ExpenseList is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
 const App = () => {
-  return <div>App</div>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 };
 
 export default App;
