59d9fbe1471176480e573ff81cd188e787d9ad7d
ri-commits <kartashourychard@gmail.com>
2023-04-25 08:34:26 +0200

    fetching the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
@@ -7,6 +7,13 @@ import ExpenseList2 from "./expense-tracker/components/ExpenseList2";
 import ExpenseFilter2 from "./expense-tracker/components/ExpenseFilter2";
 import ExpenseForm2 from "./expense-tracker/components/ExpenseForm2";
 import FormZod from "./zod-form/components/FormZod";
+import Back from "./connectingBck/components/Back";
+import ProductList from "./connectingBck/components/ProductList";
+import axios, { AxiosError, CanceledError } from "axios";
+import { literal } from "zod";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconecting");
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -31,8 +38,92 @@ const App = () => {
     ? expenses2.filter((expence) => expence.category === selectedCategory2)
     : expenses2;
 
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+
+  interface User {
+    id: number;
+    name: string;
+  }
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <div>
+      <div className="mb-3">
+        {error && <p className="text-danger">{error}</p>}
+        {isLoading && <div className="spinner-border"></div>}
+        <ul className="list-group">
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className="list-group-item d-flex justify-content-between"
+            >
+              {user.name}
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="mb-3">
+        <select
+          className="form-select"
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value=""></option>
+          <option value="Closing">Closing</option>
+          <option value="Household ">Household </option>
+        </select>
+        <ProductList category={category} />
+      </div>
+      <div className="mb-3">
+        <Back />
+      </div>
       <div className="mb-5">
         <FormZod />
       </div>
---
+++ src/connectingBck/components/Back.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+const Back = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  //   afterRender
+  useEffect(() => {
+    //   Side effect
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+  return (
+    <div>
+      <input ref={ref} type="text" className="form-control" />
+    </div>
+  );
+};
+
+export default Back;
